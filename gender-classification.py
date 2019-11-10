1ee15a1f9db9c00fbf3259239fa0c40f2b6f9af3
Elizabeth Tenorio <betsky98@gmail.com>
2019-11-09 23:21:35 -0500

    refactor to report validation set results

--- gender-classification.py
+++ gender-classification.py
@@ -18,6 +18,29 @@ def gender_features(name):
     return features
 
 
+def report(clf, train_set, val_set):
+    print('Train Set Accuracy:', nltk.classify.accuracy(clf, train_set))
+    print('Validation Set Accuracy:', nltk.classify.accuracy(clf, val_set))
+    print(clf.show_most_informative_features(3))
+
+    male_characters = ['Eddard', 'Robb',' Jon', 'Bran', 'Jaime', 'Joffrey', 'Tyrion', 'Theon', 'Varys', 'Viserys']
+    female_characters = ['Catelyn', 'Sansa', 'Arya', 'Cersei', 'Daenerys', 'Melisandre', 'Margaery', 'Brienne', 'Gilly', 'Missandei']
+
+    true_males = 0
+    for name in male_characters:
+        if clf.classify(gender_features(name)) == 'male':
+            true_males += 1
+
+    true_females = 0
+    for name in female_characters:
+        if clf.classify(gender_features(name)) == 'female':
+            true_females += 1
+
+    print('Gender classification of Game of Thrones characters:')
+    print('Males: {} out of 10 were correctly classified'.format(true_males))
+    print('Females: {} out of 10 were correctly classified'.format(true_females))
+
+
 def inspect_errors(val_names):
     errors = []
     for (name, tag) in val_names:
@@ -40,44 +63,17 @@ test_set = [(gender_features(n), gender) for (n, gender) in test_names]
 
 # 1. Naive Bayes + last letter
 # Test Set Accuracy: 0.79
-# Most Informative Features
-#            last_letter = 'a'            female : male   =     36.8 : 1.0
-#            last_letter = 'k'              male : female =     31.3 : 1.0
-#            last_letter = 'f'              male : female =     26.6 : 1.0
-#
 # Gender classification of Game of Thrones characters:
 # Males: 8 out of 10 were correctly classified
 # Females: 8 out of 10 were correctly classified
 
 # 2. Naive Bayes + last letter + length and extra features
-clf = nltk.NaiveBayesClassifier.train(train_set)
-print('Train Set Accuracy:', nltk.classify.accuracy(clf, train_set))
-Train Set Accuracy: 0.787
-print('Test Set Accuracy:', nltk.classify.accuracy(clf, test_set))
-Test Set Accuracy: 0.786
-
-print(clf.show_most_informative_features(3))
-# Most Informative Features
-#            last_letter = 'k'              male : female =     44.0 : 1.0
-#            last_letter = 'a'            female : male   =     35.4 : 1.0
-#            last_letter = 'f'              male : female =     16.0 : 1.0
-
-male_characters = ['Eddard', 'Robb',' Jon', 'Bran', 'Jaime', 'Joffrey', 'Tyrion', 'Theon', 'Varys', 'Viserys']
-female_characters = ['Catelyn', 'Sansa', 'Arya', 'Cersei', 'Daenerys', 'Melisandre', 'Margaery', 'Brienne', 'Gilly', 'Missandei']
-
-true_males = 0
-for name in male_characters:
-    if clf.classify(gender_features(name)) == 'male':
-        true_males += 1
-
-true_females = 0
-for name in female_characters:
-    if clf.classify(gender_features(name)) == 'female':
-        true_females += 1
-
-print('Gender classification of Game of Thrones characters:')
-print('Males: {} out of 10 were correctly classified'.format(true_males))
-print('Females: {} out of 10 were correctly classified'.format(true_females))
 # Gender classification of Game of Thrones characters:
 # Males: 9 out of 10 were correctly classified
 # Females: 9 out of 10 were correctly classified
+
+# 3. Naive Bayes#2 + prefixes/suffixes
+clf = nltk.NaiveBayesClassifier.train(train_set)
+report(clf, train_set, val_set)
+print(inspect_errors(val_names))
+
